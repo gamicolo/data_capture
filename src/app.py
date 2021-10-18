c2b675551dbc2bff2f10f7d9625d45089cd612f6
gamicolo <guillermoamicolo@gmail.com>
2021-10-18 14:37:36 -0300

    feat: add doc to methods

--- src/app.py
+++ src/app.py
@@ -8,11 +8,13 @@ logger=logging.getLogger('DataCapture')
 class DataCapture():
 
     def __init__(self):
+
         self.numbers = []
         self.less_values = {}
         self.greater_values = {}
 
     def add(self,num):
+
         if type(num) == int:
             self.numbers.append(num)
 
@@ -43,12 +45,20 @@ class DataCapture():
         return Stats(self.numbers, self.less_values, self.greater_values)
 
     def _get_max_item_value(self,numbers):
+        
+        '''Internal method to get the max value of te collection (must be sorted from high to low)'''
         return numbers[len(numbers)-1]
 
     def _get_max_item_index(self, i):
+        
+        '''Internal method to get the max value of the collection'''
+
         return len(self.numbers) - self.numbers[::-1].index(i)
 
     def _get_next_values(self, i):
+
+        '''Internal method to get the less and greater value of an iterator i from the collection of numbers'''
+
         logger.debug('_get_next_values, i: %s' % i)
         greater = 0
         less = 0
@@ -69,6 +79,7 @@ class DataCapture():
 class Stats():
 
     def __init__(self,numbers,less_values, greater_values):
+
         self.numbers = numbers
         self.max_less_value = len(numbers)
         self.less_values = less_values
@@ -86,6 +97,8 @@ class Stats():
 
     def _between(self, num_1, num_2):
 
+        ''' Method not working'''
+
         if not(type(num) == int):
             return 0
         if num_1 > num_2:
@@ -97,6 +110,10 @@ class Stats():
 
     def between(self,num_1,num_2):
 
+        if not(type(num) == int):
+            return 0
+        if num_1 > num_2:
+            return 0
         amount = len([i for i in self.numbers if num_1 <= i <= num_2])
         logger.debug('The amount of values between the numbers <%s> and <%s> is <%s>' % (str(num_1),str(num_2),str(amount)))
         return amount
