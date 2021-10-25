810591c7fe2b2ff4fe5f0adda940046149f28403
gamicolo <guillermoamicolo@gmail.com>
2021-10-25 11:07:34 -0300

    feat: add tests to match with the new between method

--- test/app_test.py
+++ test/app_test.py
@@ -54,22 +54,24 @@ class TestDataCapture(unittest.TestCase):
 
         self.assertEqual(self.dc_sorted._get_less_values(10),(9,9))
 
-
     def test_build_stats(self):
 
         expected_less_values = {0:0,1:0,2:0,3:0,4:2,5:3,6:3,7:4,8:4,9:4}
+        expected_less_equal_values = {0:0,1:0,2:0,3:2,4:3,5:3,6:4,7:4,8:4,9:5}
         expected_greater_values = {0:5,1:5,2:5,3:3,4:2,5:2,6:1,7:1,8:1,9:0}
         stats = self.dc.build_stats()
+
         self.assertEqual(stats.less_values,expected_less_values)
+        self.assertEqual(stats.less_equal_values,expected_less_equal_values)
         self.assertEqual(stats.greater_values,expected_greater_values)
 
 class TestStats(unittest.TestCase):
 
     def setUp(self):
 
-        self.stats = Stats([3,9,3,4,6], {0:0,1:0,2:0,3:0,4:2,5:3,6:3,7:4,8:4,9:4}, {0:5,1:5,2:5,3:3,4:2,5:2,6:1,7:1,8:1,9:0})
+        self.stats = Stats([3,9,3,4,6], {0:0,1:0,2:0,3:0,4:2,5:3,6:3,7:4,8:4,9:4}, {0:0,1:0,2:0,3:2,4:3,5:3,6:4,7:4,8:4,9:5}, {0:5,1:5,2:5,3:3,4:2,5:2,6:1,7:1,8:1,9:0})
 
-    def test_less(self):
+    def test_less_ok(self):
 
         self.assertEqual(self.stats.less(4),2)
 
@@ -85,9 +87,17 @@ class TestStats(unittest.TestCase):
 
         self.assertEqual(self.stats.less(3),0)
 
-    def test_between(self):
+    def test_between_ok(self):
 
         self.assertEqual(self.stats.between(3,6),4)
+
+    def test_between_another_ok(self):
+
+        self.assertEqual(self.stats.between(4,9),3)
+
+    def test_between_not_in_less_dicts(self):
+
+        self.assertEqual(self.stats.between(10,12),0)
 
     def test_between_first_not_int(self):
 
@@ -101,7 +111,7 @@ class TestStats(unittest.TestCase):
 
         self.assertEqual(self.stats.between(6,3),0)
 
-    def test_greater(self):
+    def test_greater_ok(self):
 
         self.assertEqual(self.stats.greater(4),2)
 
